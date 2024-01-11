9be040e523a2410945b324393d0eef318cedc596
Juniper <juniperwrenarts@gmail.com>
2024-01-11 11:40:28 -0500

    added logic to generate readme in command line

--- index.js
+++ index.js
@@ -1,13 +1,98 @@
-// TODO: Include packages needed for this application
+//  Included packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What is this project named?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'What is a short description of the project?',
+  },
+  {
+    type: 'checkbox',
+    message: 'What would you like to include in your table of contents?',
+    name: 'tableOfContents',
+    choices: ['Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions'],
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information',
+  },
+  {
+    type: 'list', 
+    name: 'license',
+    message: 'Choose a license for your project',
+    choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide instructions on how you would like others to contribute',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide instructions on how to run tests:',
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'Provide details on how others can contact you to ask questions.',
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// promt user with questions
+inquirer.prompt(questions).then((data) => {
+// generate a filename for the README based on the project name
+  const filename = `${data.projectName.toLowerCase().split(' ').join('')}.README.md`;
 
-// TODO: Create a function to initialize app
-function init() {}
+// generate the content for the README using the gathered data
+  const readmeContent = `
+# ${data.projectName}
 
-// Function call to initialize app
-init();
+## Description
+${data.description}
+
+## Table of Contents
+${data.tableOfContents.join('\n')}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact me at ${data.questions}.
+`;
+
+// write the generated content to a file with the generated filename
+  fs.writeFile(filename, readmeContent, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`${filename} created successfully!`);
+    }
+  });
+});
